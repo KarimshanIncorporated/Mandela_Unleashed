804760b23e49b5e9ba5d156487da397e6c54d1eb
arydoodle <aryansefidi@gmail.com>
2014-04-19 00:11:58 -0500

    Create Mandela_Unleashed.java

---
+++ Mandela_Unleashed.java
@@ -0,0 +1,27 @@
+/*
+ * Mandela Unleashed
+ * A text adventure
+ * A collaborative effort by Johnny, Aryan, and Joseph
+ * 4/18/2014
+ */
+
+package mandela_unleashed;
+import java.util.Scanner;
+/**
+ *
+ * @author Ary of Doodle
+ */
+public class Mandela_Unleashed 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+    //declare variables
+    Scanner in = new Scanner(System.in);
+    
+    }
+    
+}
