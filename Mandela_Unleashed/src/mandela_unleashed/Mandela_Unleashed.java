31eb7de9603b1d4d14f409d91a3195fd99cd7c47
Aryan S <aryansefidi@gmail.com>
2014-04-20 17:24:45 -0500

    Netbeans Compatible

---
+++ Mandela_Unleashed/src/mandela_unleashed/Mandela_Unleashed.java
@@ -0,0 +1,25 @@
+/*
+ * Mandela_Unleashed    
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mandela_unleashed;
+
+/**
+ *
+ * @author Ary of Doodle
+ */
+public class Mandela_Unleashed 
+
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+    }
+    
+}
